207de591109529e2ff8ceabc02a2e961c217a059
basair-quran-coder <ebrahim_alshal@hotmail.com>
2026-09-04 15:54:26 +0400

    منع تداخل عناوين السور والبسملة مع الآيات

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'basair-mushaf-complete-v6-3';
+const CACHE = 'basair-mushaf-complete-v6-4';
 const CORE = ['./', './index.html', './mushaf-v2.css', './mushaf-v2.js', './questions.js', './amiri-quran.ttf', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
