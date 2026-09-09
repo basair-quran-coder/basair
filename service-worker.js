f1f7e6c73879046f3cb0694fd9dd16241fa1971b
basair-quran-coder <ebrahim_alshal@hotmail.com>
2026-09-09 14:13:49 +0400

    إضافة المصحف الصوتي وتنزيل تلاوات القراء

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,13 @@
-const CACHE = 'basair-mushaf-complete-v6-13';
+const CACHE = 'basair-mushaf-complete-v6-14';
+const AUDIO_CACHE = 'basair-quran-audio-v1';
 const CORE = ['./', './index.html', './mushaf-v2.css', './mushaf-v2.js', './questions.js', './page-layout.js', './amiri-quran.ttf', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE && key !== AUDIO_CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const sameOrigin = new URL(event.request.url).origin === self.location.origin;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (!sameOrigin) return response;
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
     return response;
